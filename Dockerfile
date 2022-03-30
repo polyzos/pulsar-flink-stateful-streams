fccbeafce75cad7b8a447344f9dd71bf5aeeb24f
ipolyzos <ipolyzos.se@gmail.com>
2022-03-30 10:27:56 +0300

    add docker-compose file

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-#FROM apachepulsar/pulsar:2.8.0 as pulsar
-FROM apachepulsar/pulsar:2.9.1 as pulsar
-
-ENV PULSAR_HOME=/pulsar
-
-COPY conf/standalone.conf $PULSAR_HOME/conf/standalone.conf
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 mvn clean package
 
-flink run \
+docker cp \
+  target/pulsar-flink-stateful-streams-0.1.0.jar \
+  pulsar-flink-stateful-streams_taskmanager_1:opt/flink/job.jar
+
+docker exec -it pulsar-flink-stateful-streams_taskmanager_1 ./bin/flink run \
 --class io.ipolyzos.compute.EnrichmentStream \
-  target/pulsar-flink-stateful-streams-0.1.0.jar
+  job.jar
